7decb2bcf3fa6bf2516d9b7b3d93275587655ba5
BruckbauerA <alexander.bruckbauer@eu.panasonic.com>
2019-02-08 18:37:43 +0100

    latest changes

--- src/main/java/edu/glyndwr/validator/frontend/factories/UniversityRecordTableViewFactory.java
+++ src/main/java/edu/glyndwr/validator/frontend/factories/UniversityRecordTableViewFactory.java
@@ -7,10 +7,8 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableColumn.CellEditEvent;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.control.cell.TextFieldTableCell;
 import lombok.Getter;
 import org.springframework.stereotype.Component;
 
